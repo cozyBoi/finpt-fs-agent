e305ab60da6ad1fd9e2daebc4d675f6ca2894fc7
jlee.air <kjjh1126@gmail.com>
2024-01-16 14:12:36 +0900

    [feat] 기업이름 및 코드 가져오는 코드 완성

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from request_agent import request_agent
-import io
-import zipfile
-import yaml
+from corpcode import corpcode
 
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-api_key = config['api_key']
-url = config['url']
-
-ra = request_agent(api_key)
-res = ra.get(url)
-if res.status_code == 200:
-    with zipfile.ZipFile(io.BytesIO(res.content)) as z:
-        z.extractall()
-        print("success")
-else:
-    print("failed")
+cc = corpcode()
+print(cc.findWithName('삼성전자'))
