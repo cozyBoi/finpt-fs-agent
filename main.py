a26a04e79475b2235b494b727cb8cccd3ff759a3
jlee.air <kjjh1126@gmail.com>
2024-01-16 19:56:02 +0900

    [feat] 사업보고서 받아오는 코드 추가

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from lib.corpcode import corpcode
+from lib.request_agent import request_agent
+import os.path
+import yaml
+
+with open(os.path.dirname(__file__) + '/static/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+api_key = config['api_key']
+
 cc = corpcode()
-print(cc.findWithName('삼성전자'))
+corpInfo = cc.findWithName('삼성전자')
+ra = request_agent(api_key)
+ra.add_param("corp_code", corpInfo["corp_code"])
+ra.add_param("bsns_year", "2023")
+ra.add_param("reprt_code", "11014")
+ra.add_param("idx_cl_code", "M210000")
+print(ra.get("https://opendart.fss.or.kr/api/fnlttSinglIndx.json").content.decode("utf-8"))
